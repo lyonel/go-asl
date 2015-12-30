67853f95f03a19b3f02a44ddbc30e67e89570016
Lyonel Vincent <lyonel@ezix.org>
2015-12-30 01:18:04 +0100

    more work on ASL interface

--- asl.go
+++ asl.go
@@ -7,8 +7,8 @@ package asl
 */
 import "C"
 import (
+	"errors"
 	"fmt"
-	"sync"
 	"unsafe"
 )
 
@@ -46,14 +46,24 @@ const (
 	QUERY_OP_NUMERIC       = int(C.ASL_QUERY_OP_NUMERIC)
 )
 
-type Client struct {
+type Object struct {
 	asl_object C.asl_object_t
-	mu         sync.Mutex
+}
+
+type Client struct {
+	Object
 }
 
 type Query struct {
-	asl_object C.asl_object_t
-	mu         sync.Mutex
+	Object
+}
+
+type Response struct {
+	Object
+}
+
+type Message struct {
+	Object
 }
 
 func Open(ident, facility string, opts int) (*Client, error) {
@@ -61,30 +71,96 @@ func Open(ident, facility string, opts int) (*Client, error) {
 	defer C.free(unsafe.Pointer(i))
 	f := C.CString(facility)
 	defer C.free(unsafe.Pointer(f))
-	c := &Client{}
-	c.asl_object = C.asl_open(i, f, C.uint32_t(opts))
 
-	if c.asl_object != nil {
-		return c, nil
-	} else {
-		return nil, fmt.Errorf("Failed to create client: %d", c.asl_object)
+	client := &Client{}
+	if ident == "" {
+		i = nil
 	}
-}
+	if facility == "" {
+		f = nil
+	}
+	client.asl_object = C.asl_open(i, f, C.uint32_t(opts))
 
-func (client *Client) Close() error {
-	client.mu.Lock()
-	C.asl_close(client.asl_object)
-	client.mu.Unlock()
-	return nil
+	if client.asl_object != nil {
+		return client, nil
+	} else {
+		return nil, errors.New("Failed to create client")
+	}
 }
 
 func NewQuery() (*Query, error) {
-	q := &Query{}
-	q.asl_object = C.asl_new(C.ASL_TYPE_QUERY)
+	query := &Query{}
+	query.asl_object = C.asl_new(C.ASL_TYPE_QUERY)
 
-	if q.asl_object != nil {
-		return q, nil
+	if query.asl_object != nil {
+		return query, nil
 	} else {
-		return nil, fmt.Errorf("Failed to create query: %d", q.asl_object)
+		return nil, errors.New("Failed to create query")
 	}
+}
+
+func (query *Query) SetQuery(key string, value interface{}, flags int) {
+	k := C.CString(key)
+	defer C.free(unsafe.Pointer(k))
+	v := C.CString(fmt.Sprintf("%v", value))
+	defer C.free(unsafe.Pointer(v))
+
+	switch value.(type) {
+	case int, uint, int32, uint32, int64, uint64, float32, float64:
+		flags |= QUERY_OP_NUMERIC
+	case nil:
+		v = nil
+	}
+
+	C.asl_set_query(query.asl_object, k, v, C.uint32_t(flags))
+}
+
+func (client *Client) Search(query Query) *Response {
+	response := &Response{}
+	response.asl_object = C.asl_search(client.asl_object, query.asl_object)
+
+	return response
+}
+
+func (response *Response) Next() *Message {
+	if m := C.asl_next(response.asl_object); m != nil {
+		message := &Message{}
+		message.asl_object = m
+		return message
+	} else {
+		return nil
+	}
+}
+
+func (message *Message) Key(i int) string {
+	if v := C.asl_key(message.asl_object, C.uint32_t(i)); v != nil {
+		return C.GoString(v)
+	} else {
+		return ""
+	}
+}
+
+func (message *Message) Get(key string) string {
+	k := C.CString(key)
+	defer C.free(unsafe.Pointer(k))
+	if v := C.asl_get(message.asl_object, k); v != nil {
+		return C.GoString(v)
+	} else {
+		return ""
+	}
+}
+
+func (object *Object) Close() error {
+	C.asl_close(object.asl_object)
+	return nil
+}
+
+func (object *Object) Free() error {
+	C.asl_free(object.asl_object)
+	return nil
+}
+
+func (object *Object) Release() error {
+	C.asl_release(object.asl_object)
+	return nil
 }
